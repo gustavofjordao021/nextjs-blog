d9ccb5a0eff570a6b262c6a3c9dd2ebab5899d3f
Gustavo Jordao <gustavo.jordao021@gmail.com>
2022-03-21 14:20:18 -0400

    Attempt to trigger redeploy

--- pages/components/date.js
+++ pages/components/date.js
@@ -2,6 +2,5 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  console.log(dateString);
   return <time>{format(date, "MMMM d, yyyy")}</time>;
 }
